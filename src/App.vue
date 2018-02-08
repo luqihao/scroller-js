d4b7a58171c93dcb8aee54d70c7021cee1219842
luqihao <luqihao@liyuanheng.com>
2018-02-08 16:25:05 +0800

    '添加了对可编辑类型容器的适配,但未对dom结构发生变化后可编辑容器的scroller重定位'

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,7 @@
 <template>
   <div id="app">
-    <ul class="scroll" id="scroller" v-html="msg">
-      <!-- {{msg}} -->
-    </ul>
-  </div>
-</template>
-
-<script>
-import Scroller from './assets/scroller.js'
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  },
-  mounted () {
-    this.scroller = new Scroller({
-      el: 'scroller',
-      color: 'rgb(55, 155, 255)',
-      radius: true
-    })
-    // console.log(document.getElementById('scroller').scrollHeight)
-    // console.log(document.getElementById('scroller').offsetHeight)
-    // console.log(document.getElementById('scroller').clientHeight)
-    // console.log(document.getElementById('scroller').clientTop)
-    // console.log(document.getElementById('scroller').scrollTop)
-    // console.log(document.getElementById('scroller').offsetTop)
-    // setTimeout(() => {
-    //   document.getElementById('scroller').innerHTML +=
-    //   `<li>1</li>
-    //   <li>2</li>
-    //   <li>3</li>
-    //   <li>4</li>
-    //   <li>5</li>
-    //   <li>6</li>
-    //   <li>7</li><li>8</li><li>9</li><li>10</li><li>11</li><li>12</li>`
-    //   this.scroller.reset()
-    // }, 2000)
-    setTimeout(() => {
-      this.msg = `<li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>7</li><li>8</li><li>9</li><li>10</li><li>11</li><li>12</li>`
-      this.$nextTick(() => {
-        this.scroller.init()
-      })
-    }, 2000)
-    setTimeout(() => {
-      this.msg = `<li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>7</li><li>8</li><li>9</li><li>10</li><li>11</li><li>12</li>
+    <ul class="scroll" id="scroller">
       <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>7</li><li>8</li><li>9</li><li>10</li><li>11</li><li>12</li>`
-      this.$nextTick(() => {
-        this.scroller.init()
-      })
-    }, 4000)
-    setTimeout(() => {
-      this.msg = `<li>1</li>
       <li>2</li>
       <li>3</li>
       <li>4</li>
@@ -96,12 +27,72 @@ export default {
       <li>4</li>
       <li>5</li>
       <li>6</li>
-      <li>7</li><li>8</li><li>9</li><li>10</li><li>11</li><li>12</li>`
-      this.$nextTick(() => {
-        this.scroller.init()
-      })
-    }, 6000)
-    // console.log(document.getElementById('scroller').clientHeight / document.getElementById('scroller').scrollHeight)
+      <li>7</li><li>8</li><li>9</li><li>10</li><li>11</li><li>12</li>
+    </ul>
+    <textarea id="textarea" name="name" rows="8" cols="80">
+      1
+      2
+      3
+      4
+      5
+      6
+      7
+      8
+      9
+      10
+      11
+      12
+      13
+      14
+      15
+      16
+      17
+      18
+      19
+      20
+      21
+      22
+      23
+      24
+      25
+      26
+      27
+      28
+      29
+      30
+    </textarea>
+  </div>
+</template>
+
+<script>
+import Scroller from './assets/scroller.js'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted () {
+    // this.scroller = new Scroller({
+    //   el: 'scroller',
+    //   color: 'rgb(55, 155, 255)',
+    //   radius: true
+    // })
+    this.scroller = new Scroller({
+      el: 'textarea',
+      color: 'rgb(55, 155, 255)',
+      radius: true
+    })
+    setTimeout(() => {
+      document.getElementById('scroller').style.maxHeight = '300px'
+    }, 5000)
+    // console.log(document.getElementById('scroller').scrollHeight)
+    // console.log(document.getElementById('scroller').offsetHeight)
+    // console.log(document.getElementById('scroller').clientHeight)
+    // console.log(document.getElementById('scroller').clientTop)
+    // console.log(document.getElementById('scroller').scrollTop)
+    // console.log(document.getElementById('scroller').offsetTop)
   }
 }
 </script>
@@ -110,6 +101,10 @@ export default {
 ul {
   /* transform: translate(0, 0); */
   border: 1px solid #000;
+  max-height: 200px;
+  overflow: hidden;
+}
+textarea {
   max-height: 200px;
   overflow: hidden;
 }
--- src/assets/scroller.js
+++ src/assets/scroller.js
@@ -1,20 +1,57 @@
-import './scroller.css'
+// import './scroller.css'
 export default class Scroller {
   constructor({el, color, radius}) {
     this.ele = document.getElementById(el)
-    this.color = color
+    this.color = color || 'rgba(0, 0, 0, .5)'
     this.radius = radius
     this.draggable = false
+    this.type = /INPUT|SELECT|TEXTAREA/i.test(this.ele.nodeName)
     this.init()
   }
   init () {
     if (this.ele.clientHeight < this.ele.scrollHeight) {
-      this.createScroller()
+      if (!this.type) this.createInsetScroller()
+      else this.createOutsetScroller()
     } else {
       console.log('can not create Scroller')
     }
   }
-  createScroller () {
+  createOutsetScroller () {
+    if (!document.getElementById(`${this.ele.id}-scroller-wrapper`)) {
+      let wrapper = document.createElement('div')
+      let thumb = document.createElement('div')
+
+      wrapper.appendChild(thumb)
+      document.body.appendChild(wrapper)
+
+      wrapper.setAttribute('id', `${this.ele.id}-scroller-wrapper`)
+      thumb.setAttribute('id', `${this.ele.id}-scroller-thumb`)
+
+      wrapper.setAttribute('class', 'scroller-wrapper')
+      thumb.setAttribute('class', 'scroller-thumb')
+
+      let x = this.ele.getBoundingClientRect().x,
+          width = this.ele.getBoundingClientRect().width,
+          height = this.ele.getBoundingClientRect().height,
+          top = this.ele.getBoundingClientRect().top,
+          wrapperWidth = 5
+      this.ele.style.resize = 'none'
+      wrapper.setAttribute('style', `position: absolute; left: ${x + width - wrapperWidth}px; top: ${top}px; z-index: 10; width: 5px; height: ${height}px; background-color: rgba(0, 0, 0, .1); opacity: 0; transition: opacity 1s;`)
+      thumb.setAttribute('style', `background-color: ${this.color}; height: ${this.ele.clientHeight * (this.ele.clientHeight / this.ele.scrollHeight)}px; width: 100%; cursor: pointer;`)
+
+      if (this.radius) thumb.style.borderRadius = '10px'
+
+      this.wrapper = wrapper
+      this.thumb = thumb
+      this.hover()
+      this.scroll()
+      this.drag()
+      this.change()
+    } else {
+      this.thumb.style.height = `${this.ele.clientHeight * (this.ele.clientHeight / this.ele.scrollHeight)}px`
+    }
+  }
+  createInsetScroller () {
     if (!document.getElementById(`${this.ele.id}-scroller-wrapper`)) {
       let wrapper = document.createElement('div')
       let thumb = document.createElement('div')
@@ -31,7 +68,8 @@ export default class Scroller {
       thumb.setAttribute('class', 'scroller-thumb')
 
       this.ele.setAttribute('style', 'position: relative;')
-      thumb.setAttribute('style', `background-color: ${this.color}; height: ${this.ele.clientHeight * (this.ele.clientHeight / this.ele.scrollHeight)}px`)
+      wrapper.setAttribute('style', 'position: absolute; right: 0; top: 0; z-index: 10; width: 5px; height: 100%; background-color: rgba(0, 0, 0, .1); opacity: 0; transition: opacity 1s;')
+      thumb.setAttribute('style', `background-color: ${this.color}; height: ${this.ele.clientHeight * (this.ele.clientHeight / this.ele.scrollHeight)}px; width: 100%; cursor: pointer;`)
 
       if (this.radius) thumb.style.borderRadius = '10px'
 
@@ -44,16 +82,26 @@ export default class Scroller {
       this.thumb.style.height = `${this.ele.clientHeight * (this.ele.clientHeight / this.ele.scrollHeight)}px`
     }
   }
-  hover () {
+  change () {
+    this.ele.addEventListener('input', () => {
+      this.wrapper.style.opacity = '1'
+      this.thumb.style.height = `${this.ele.clientHeight * (this.ele.clientHeight / this.ele.scrollHeight)}px`
+      this.thumb.style.transform = `translateY(${this.ele.scrollTop / (this.ele.scrollHeight / this.ele.clientHeight)}px)`
+    })
+  }
+  hover (wrapper) {
     this.ele.addEventListener('mouseover', this.mouseover.bind(this), false)
     this.ele.addEventListener('mouseout', this.mouseout.bind(this), false)
+
+    this.wrapper.addEventListener('mouseover', this.mouseover.bind(this), false)
+    this.wrapper.addEventListener('mouseout', this.mouseout.bind(this), false)
   }
   mouseover (event) {
     event.preventDefault()
     this.wrapper.style.opacity = '1'
   }
   mouseout (event) {
-    event.preventDefault()
+    // event.preventDefault()
     if (!this.draggable) this.wrapper.style.opacity = '0'
   }
   scroll (clientHeight, scrollHeight) {
@@ -65,11 +113,11 @@ export default class Scroller {
     let num = event.wheelDelta || event.detail
     if (num === 120 || num === -3) {
       this.ele.scrollTop -= this.ele.clientHeight / 10
-      this.wrapper.style.transform = `translateY(${this.ele.scrollTop}px)`
+      if (!this.type) this.wrapper.style.transform = `translateY(${this.ele.scrollTop}px)`
       this.thumb.style.transform = `translateY(${this.ele.scrollTop / (this.ele.scrollHeight / this.ele.clientHeight)}px)`
     } else if (num === -120 || num === 3) {
       this.ele.scrollTop += this.ele.clientHeight / 10
-      this.wrapper.style.transform = `translateY(${this.ele.scrollTop}px)`
+      if (!this.type) this.wrapper.style.transform = `translateY(${this.ele.scrollTop}px)`
       this.thumb.style.transform = `translateY(${this.ele.scrollTop / (this.ele.scrollHeight / this.ele.clientHeight)}px)`
     }
   }
@@ -88,13 +136,13 @@ export default class Scroller {
     if (this.draggable) {
       let distance = event.clientY - this.mousedownY
       this.ele.scrollTop += distance * (this.ele.scrollHeight / this.ele.clientHeight)
-      this.wrapper.style.transform = `translateY(${this.ele.scrollTop}px)`
+      if (!this.type) this.wrapper.style.transform = `translateY(${this.ele.scrollTop}px)`
       this.thumb.style.transform = `translateY(${this.ele.scrollTop / (this.ele.scrollHeight / this.ele.clientHeight)}px)`
       this.mousedownY = event.clientY
     }
   }
   mouseup (event) {
-    event.preventDefault()
+    // event.preventDefault()
     this.draggable = false
     this.mouseout()
   }
